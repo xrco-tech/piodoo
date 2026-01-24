5553cf33ab9eab843c3ad2556c578365e621ab13
HandsomeRocks <xm.radebe@gmail.com>
2026-01-25 01:15:42 +0200

    Add WA chatbot extension module

--- addons/whatsapp_ligth_chatbot/models/__init__.py
+++ addons/whatsapp_ligth_chatbot/models/__init__.py
@@ -4,7 +4,6 @@ from . import whatsapp_chatbot
 from . import whatsapp_chatbot_step
 from . import whatsapp_chatbot_contact
 from . import whatsapp_chatbot_message
-from . import whatsapp_chatbot_trigger
 from . import whatsapp_chatbot_variable
 from . import whatsapp_chatbot_answer
 
